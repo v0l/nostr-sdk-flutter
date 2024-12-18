70db2d96efac2d8d803adc64fd283bedcdcae0c1
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-12-18 11:46:38 +0100

    Delete dart output dir before regenerate it
    This will remove all unnecessary files

--- rust/build.rs
+++ rust/build.rs
@@ -3,6 +3,8 @@
 // Distributed under the MIT software license
 
 use std::process::{Command, Stdio};
+use std::fs;
+use std::path::PathBuf;
 
 use lib_flutter_rust_bridge_codegen::codegen;
 use lib_flutter_rust_bridge_codegen::codegen::Config;
@@ -17,12 +19,26 @@ fn main() {
     if !is_flutter_installed() {
         panic!("Warning: flutter not installed.");
     }
+    
+    // Parse config
+    let config = Config::from_config_file("../flutter_rust_bridge.yaml")
+        .unwrap()
+        .unwrap();
+    
+    // Delete previously generated dart code
+    if let Some(path) = &config.dart_output {
+        let dir: PathBuf = PathBuf::from("..").join(path);
+        if dir.exists() {
+            println!("Deleting {}", dir.display());
+            fs::remove_dir_all(&dir).unwrap();   
+        }
+
+        fs::create_dir_all(dir).unwrap();
+    }
 
     // Execute code generator with auto-detected config
     codegen::generate(
-        Config::from_config_file("../flutter_rust_bridge.yaml")
-            .unwrap()
-            .unwrap(),
+        config,
         Default::default(),
     )
     .unwrap();
